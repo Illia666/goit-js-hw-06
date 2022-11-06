d52082315baf66178154e45b141c3ff0f94f9154
Illia666 <108007460+Illia666@users.noreply.github.com>
2022-11-06 12:21:27 +0200

    update

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,4 @@
-/*
-Write a script that:
-Will count and display in the console the number of categories in ul#categories, that is, li.item elements.
-For each li.item element in the ul#categories list, will find and display in the console the element header text (<h2> tag) and the number of elements in the category (all nested <li>).
-As a result, the following messages will be displayed in the console.
-*/
+
 const categoryItemsEl = document.querySelectorAll("#categories .item");
 
 console.log(`Number of categories: ${categoryItemsEl.length} \n`);
